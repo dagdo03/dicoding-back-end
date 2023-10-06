d648fef9df9c5c3a4d4386c8ba3c0afb14886f87
dagdo03 <ihsan.widagdo@gmail.com>
2023-10-07 03:52:51 +0700

    update: progress sampai latian projeck notes web server

---
+++ hapi-web-server/routes.js
@@ -0,0 +1,59 @@
+const routes = [
+    {
+        method: 'GET',
+        path: '/',
+        handler: (request, h) => {
+            return 'Homepage';
+        },
+    },
+    {
+        method: '*',
+        path: '/',
+        handler: (request, h) => {
+            return 'Halaman tidak dapat diakses dengan method tersebut';
+        },
+    },
+    {
+        method: '*',
+        path: '/about',
+        handler: (request, h) => {
+            return 'Halaman tidak dapat diakses dengan method tersebut';
+        },
+    },
+    {
+        method: 'GET',
+        path: '/about',
+        handler: (request, h) => {
+            return 'About page';
+        },
+    },
+    {
+        method: 'GET',
+        path: '/hello/{name?}',
+        handler: (request, h) => {
+            const { name = 'stranger' } = request.params;
+            const {lang} = request.query;
+            if(lang === 'id'){
+                return `Hai, ${name}!`;
+            }
+            return `Hello, ${name}!`;
+        },
+    },
+    {
+        method: 'POST',
+        path: '/login',
+        handler: (request, h) => {
+            const { username, password } = request.payload;
+            return `Welcome ${username}!`;
+        },
+    },
+    {
+        method: '*',
+        path: '/{any*}',
+        handler: (request, h) => {
+            return 'Halaman tidak dapat diakses dengan method tersebut';
+        },
+    },
+];
+ 
+module.exports = routes;
